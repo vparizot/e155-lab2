9f9cd2ba329bee8e1b17bff6a79174e204b0451c
Victoria <vparizot@g.hmc.edu>
2024-09-07 13:38:21 -0700

    lab2 changing sv fie location

---
+++ FPGA/radiant_project/Lab2_mvp/source/mux.sv
@@ -0,0 +1,13 @@
+// Victoria Parizot
+// vparizot@g.hmc.edu
+// 9/6/2024
+// This code was taken from Digital Design and Computer Architecture by Harris
+
+module mux2 (
+	input logic [3:0] s1, s2,
+	input logic selector,
+	output logic [3:0] sy);
+
+	assign sy = selector? s1:s2;
+
+endmodule
